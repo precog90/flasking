9f70adc351bc9df25f9144ece0cadf250fe9fb46
asingh.crx <amrinder.singh@dimensiondata.com>
2017-11-17 14:48:22 -0500

    Playing with flask

--- flasking/app/__init__.py
+++ flasking/app/__init__.py
@@ -1,8 +1,47 @@
 from flask import Flask
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from app import config
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views
+db = SQLAlchemy(app)
 
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Amrinder'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'Avengers was so cool'
+        },
+        {
+            'author': {'nickname': 'Rahil'},
+            'body': 'Coded whole day'
+        },
+        {
+            'author': {'nickname': 'Nishank'},
+            'body': 'Ran all day'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['get', 'post'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me="%s"' % (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
 
--- flasking/app/config.py
+++ flasking/app/config.py
@@ -1,3 +1,10 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#print(SQLALCHEMY_DATABASE_URI)
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#print(SQLALCHEMY_MIGRATE_REPO)
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [{'name': 'Yahoo', 'url': 'https://me.yahoo.com'}]
